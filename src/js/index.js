edaccc4c0f684fabbfefd246fb8774532f780785
saviita <taatt2000@gmail.com>
2024-07-12 09:32:08 +0200

    update

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,18 @@
 // El styles lo importamos aquí, ya se carga después al compilar todo
 import '../scss/styles.scss';
+
+const generateNumbers = () => {
+    const uniqueNumbers = []
+
+    while (uniqueNumbers.length < 15) {
+        const randomNumber = Math.ceil(Math.random() * 20)
+
+        if(!uniqueNumbers.includes(randomNumber)) {
+            uniqueNumbers.push(randomNumber)
+        }
+    }
+
+
+    console.log(uniqueNumbers)
+}
+generateNumbers()
